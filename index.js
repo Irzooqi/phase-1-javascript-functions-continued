d3e4524ac74f60974f328588631a7e985970a889
irzooqi <yasirirzooqi@gmail.com>
2023-09-03 16:06:49 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+const mondayWork = function(activity) {
+    if (activity === undefined) {
+        return "This Monday, I will go to the office.";
+      } else {
+        return `This Monday, I will ${activity}.`;
+      }
+    }
+
+
+function hoistingFun(){
+   return console.log("this is hoisting function");
+}
+
+(function () {
+     console.log("Yet more razzling");
+  });
+
+  function wrapAdjective(outerSymbol) {
+    return function(adjective) {
+      return `You are ${outerSymbol}${adjective}${outerSymbol}!`;
+    };
+  }
